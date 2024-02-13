966f3eb1787ba35238d63ac8f539c61312cc8ed2
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-13 23:29:22 +0600

    generating random alphabate

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -7,7 +7,12 @@
 //   playgroundSection.classList.remove("hidden");
 // }
 
+function continueGame() {
+  // generate a random alphabate
+}
+
 function play() {
   hideElementById("home-screen");
   showElementById("playground-screen");
+  continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -7,3 +7,9 @@ function showElementById(elementId) {
   const element = document.getElementById(elementId);
   element.classList.remove("hidden");
 }
+
+function getRandomAlphabet() {
+  const alphabateString = "abcdefghijklmnopqrstuvwxyz";
+  const alphabets = alphabateString.split("");
+  console.log(alphabets);
+}
