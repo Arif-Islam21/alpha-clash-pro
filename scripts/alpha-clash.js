5b0543342fb652782cd4414d6e6b4b9bc5bcdea8
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-02-13 23:15:08 +0600

    first section done in a wey... let's check another way

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,1 +1,8 @@
-function play() {}
+function play() {
+  // hide home screen
+  const homeSection = document.getElementById("home-screen");
+  homeSection.classList.add("hidden");
+  // show the playground
+  const playgroundSection = document.getElementById("playground-screen");
+  playgroundSection.classList.remove("hidden");
+}
